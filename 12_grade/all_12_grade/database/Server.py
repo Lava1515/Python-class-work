6a8e0ef6373b916501a2b3ccecef53fb8090f41d
Your Name liav <liav.kolet@gmail.com>
2023-11-05 21:03:26 +0200

    i think i finish

--- 12_grade/all_12_grade/database/Server.py
+++ 12_grade/all_12_grade/database/Server.py
@@ -1,112 +1,159 @@
-import os
 import json
-import socket
 import threading
+from os import path
 import multiprocessing
-from typing import Hashable
-
-from protocol import Protocol
 
 
 class DataBase:
-    def __init__(self, filename):
-        self.filename = filename
-        self.data_list = {}
+    def __init__(self, D_data=None):
+        if D_data is None:
+            D_data = {}
+        self.D_data = D_data
 
-    def set_value(self, key: Hashable, data):
-        if os.path.isfile(self.filename) is False:
-            raise Exception("File not found")
+    def set_value(self, key_, data_):
+        self.D_data[key_] = data_
+
+    def get_value(self, key_):
+        if key_ in self.D_data:
+            return self.D_data[key_]
+        return "key not found"
+
+    def delete_value(self, key_):
+        if key_ in self.D_data:
+            value = self.D_data.pop(key_)
+            return value
+        return None
+
+
+class FileManager(DataBase):
+    def __init__(self):
+        self.file_path = "data.json"
+        D_data = {}
         try:
-            with open(self.filename) as fp:
-                self.data_list = json.load(fp)
+            with open(self.file_path) as f:
+                D_data = json.load(f)
         except Exception:
             pass
-        # the file is Empty
-        self.data_list.update({key: data})
-        with open(self.filename, 'w') as json_file:
-            json.dump(self.data_list, json_file,
+        super().__init__(D_data)
+        self.T_lock = threading.Lock
+
+    def set_value(self, key_, data_):
+        if path.isfile(self.file_path) is False:
+            raise Exception("File not found")
+        super().set_value(key_, data_)
+        with open(self.file_path, 'w') as json_file:
+            json.dump(self.D_data, json_file,
                       indent=4,
                       separators=(',', ': '))
 
-    def get_data(self, key: Hashable):
-        try:
-            with open(self.filename) as fp:
-                self.data_list = json.load(fp)
-        except Exception:
-            print("the file is Empty")
-        try:
-            return self.data_list[key]
-        except KeyError:
-            return "The key you entered is not valid"
-
-    def delete_value(self, key: Hashable):
-        with open(self.filename) as fp:
-            self.data_list = json.load(fp)
-
-        if key in self.data_list:
-            del self.data_list[key]
-            print("The Deleted key is:", key)
-
-        with open(self.filename, 'w') as json_file:
-            json.dump(self.data_list, json_file,
+    def delete_value(self, key_):
+        if path.isfile(self.file_path) is False:
+            raise Exception("File not found")
+        super().delete_value(key_)
+        with open(self.file_path, 'w') as json_file:
+            json.dump(self.D_data, json_file,
                       indent=4,
                       separators=(',', ': '))
 
 
-# DB = DataBase('data.json')
-# DB.set_value("aaaddag", "hgjkhf")
-# print(DB.get_data("aaag"))
-# DB.delete_value("aaag")
-#
+class Commands:
+    def __init__(self, type_):
+        self.lock = None
+        self._semaphore = None
+        self.max_readers = 10
+        self.fm = FileManager()
+        if type_ == "thread":
+            self.lock = threading.Lock()
+            self._semaphore = threading.Semaphore(self.max_readers)
+        elif type_ == "multi":
+            self.lock = multiprocessing.Lock()
+            self._semaphore = multiprocessing.Semaphore(self.max_readers)
 
-class WriteData(DataBase):
+    def add_data(self):
+        key = input("the you want to add")
+        data = input("the data you want to add to the key")
+        self.lock.acquire()
+        for _ in range(self.max_readers):
+            self._semaphore.acquire()
+        self.fm.set_value(key, data)
+        self.lock.release()
+        for _ in range(self.max_readers):
+            self._semaphore.release()
+
+    def get_data(self):
+        key = input("the you want to get")
+        self.lock.acquire()
+        for _ in range(self.max_readers):
+            self._semaphore.acquire()
+        x = self.fm.get_value(key)
+        self.lock.release()
+        for _ in range(self.max_readers):
+            self._semaphore.release()
+        return x
+
+    def delete_data(self):
+        key = input("the you want to remove")
+        self.lock.acquire()
+        for _ in range(self.max_readers):
+            self._semaphore.acquire()
+            for _ in range(self.max_readers):
+                self._semaphore.acquire()
+        self.fm.delete_value(key)
+        self.lock.release()
+        for _ in range(self.max_readers):
+            self._semaphore.release()
+
+    def commands(self):
+        running = True
+        while running:
+            command = input("Enter the command you want to do from the list "
+                            "[Add | Get | Remove | quit]")
+
+            if command.lower() == "add":
+                self.add_data()
+
+            elif command.lower() == "get":
+                print(self.get_data())
+
+            elif command.lower() == "remove":
+                self.delete_data()
+
+            elif command.lower() == "quit":
+                running = False
+
+
+class ThreadManger(Commands):
     def __init__(self):
-        super().__init__('data.json')
-        self.lock_clients_sock = threading.Lock()
+        super().__init__("thread")
+        self.threads_ = []
 
-    def get_new_data(self):
-        key = input("the key you want to add")
-        data = input("the data you want to relate to the key")
-        self.lock_clients_sock.acquire()
-        self.set_value(key, data)
-        self.lock_clients_sock.release()
-
-    def remove_key(self):
-        key = input("the key you want to remove")
-        self.lock_clients_sock.acquire()
-        self.delete_value(key)
-        self.lock_clients_sock.release()
+    def start_threads(self):
+        for i in range(10):
+            t = threading.Thread(target=self.commands, daemon=True)
+            self.threads_.append(t)
+        for t in self.threads_:
+            t.start()
+            t.join()
 
 
-class Server:
+class MultiManger(Commands):
     def __init__(self):
-        self.clients_sock = {}
-        self.server_socket = Protocol()
-        self.lock_clients_sock = threading.Lock()
+        super().__init__("multi")
+        self.multi_ = []
 
-    def bind(self, ip, port):
-        self.server_socket.bind((ip, port))
-
-    def listen(self):
-        self.server_socket.listen()
-        print("Server is up and running")
-
-    def accept(self):
-        try:
-            client_sock, client_address = self.server_socket.accept()
-        except TimeoutError:
-            return
-        self.lock_clients_sock.acquire()
-        self.clients_sock[client_sock] = client_address
-        self.lock_clients_sock.release()
-        print('\r' + "[Server]: New Connection From: '%s:%s'" % client_address)
-
-    def hendle_cleints(self):
-        pass
+    def start_process(self):
+        for i in range(10):
+            p = multiprocessing.Process(target=self.commands, daemon=True)
+            self.multi_.append(p)
+        for p in self.multi_:
+            p.start()
+            p.join()
 
 
-if __name__ == '__main__':
-    server = Server()
-    server.bind("0.0.0.0", 8820)
-    server.listen()
-    server.accept()
+def main():
+    thm = ThreadManger()
+    thm.start_threads()
+
+
+if __name__ == "__main__":
+    main()
---
+++ stam/dagreeeeeee/aaa.py
@@ -0,0 +1,7 @@
+stam = ["bruh ", 1,"bruh ","bruh " "bruh ",]
+print(stam)
+stam.remove("bruh ")
+stam.pop(1)
+print(stam)
+
+
